8c2d1be79ae9c167c11954283245c7c085e72451
Yesa <ethan147852369@gmail.com>
2022-07-16 15:05:03 +0800

    1.0

--- MiniGame/Form1.Designer.cs
+++ MiniGame/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "攻擊";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 54);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "防禦";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 54);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "回魔";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(13, 85);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 44);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "PP:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(185, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Player2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Player1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Hp:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 141);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "MiniGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- MiniGame/Form1.cs
+++ MiniGame/Form1.cs
@@ -12,9 +12,138 @@ namespace MiniGame
 {
     public partial class Form1 : Form
     {
+        public int hp1, hp2, pp1, pp2, p2_choose;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Battle(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Battle(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Battle(3);
+        }
+
+        public void Print()
+        {
+            label1.Text = hp1.ToString();
+            label2.Text = pp1.ToString();
+            label3.Text = hp2.ToString();
+            label4.Text = pp2.ToString();
+        }
+
+        public void Scroll()
+        {
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+        }
+
+        public string Choose(int choose)
+        {
+            switch (choose)
+            {
+                case 1: return "攻擊";
+                case 2: return "防禦";
+                default: return "回魔";
+            }
+        }
+
+        public async void Battle(int p1_choose)
+        {
+            Random random = new Random();
+            p2_choose = random.Next(1, 4);
+            if (p2_choose != 3)
+            {
+                switch (pp2)
+                {
+                    case 5:
+                        if (random.Next(1, 7) < 3) p2_choose = 3;
+                        break;
+                    case 4:
+                        if (random.Next(1, 7) < 4) p2_choose = 3;
+                        break;
+                    case 3:
+                        if (random.Next(1, 7) < 5) p2_choose = 3;
+                        break;
+                    case 2:
+                        if (random.Next(1, 7) < 6) p2_choose = 3;
+                        break;
+                    default:
+                        if (pp2 == 0 || pp2 == 1) p2_choose = 3;
+                        break;
+                }
+            }
+            textBox1.Text += "\r\n你選擇" + Choose(p1_choose) + ",對手選擇" + Choose(p2_choose);
+            int dice1 = random.Next(1, 7), dice2 = random.Next(1, 7);
+            textBox1.Text += "\r\n擲骰中.";
+            Scroll(); await Task.Delay(1000);
+            textBox1.Text += "擲骰中..";
+            Scroll(); await Task.Delay(1000);
+            textBox1.Text += "擲骰中...";
+            Scroll(); await Task.Delay(1000);
+            textBox1.Text += "\r\n你得到的點數是" + dice1 + "\r\n對手得到的點數是" + dice2;
+            Scroll(); await Task.Delay(3000);
+            textBox1.Text += "\r\n\r\n";
+            int result1 = 0, result2 = 0;
+            if (p1_choose == 1 && p2_choose == 1)
+            {
+                if (dice1 > dice2) result2 = dice1;
+                if (dice1 < dice2) result1 = dice2;
+                textBox1.Text += "你對對手造成了" + result2 + "點傷害\r\n對手對你造成了" + result1 + "點傷害";
+            }
+            if (p1_choose == 2 && p2_choose == 2) textBox1.Text += "雙方皆損失了" + dice1 + "點能量";
+            if (p1_choose == 1 && p2_choose == 2)
+            {
+                if (dice1 > dice2) { result2 = dice1 - dice2; textBox1.Text += "你對對手造成了" + result2 + "點傷害"; }
+                if (dice1 < dice2) { result1 = dice2 - dice1; textBox1.Text += "對手對你造成了" + result1 + "點傷害，並回復了" + result1 + "點血量"; hp2 += result1; }
+                if (dice1 == dice2) textBox1.Text += "雙方皆損失了" + dice1 + "點能量";
+            }
+            if (p1_choose == 2 && p2_choose == 1)
+            {
+                if (dice1 < dice2) { result1 = dice2 - dice1; textBox1.Text += "對手對你造成了" + result1 + "點傷害"; }
+                if (dice1 > dice2) { result2 = dice1 - dice2; textBox1.Text += "你對對手造成了" + result2 + "點傷害，並回復了" + result2 + "點血量"; hp1 += result2; }
+                if (dice1 == dice2) textBox1.Text += "雙方皆損失了" + dice1 + "點能量";
+            }
+            if (p1_choose == 3 && p2_choose == 3) { textBox1.Text += "你回復了" + dice1 + "點能量\r\n對手回復了" + dice2 + "點能量"; pp1 += dice1 * 2; pp2 += dice2 * 2; }
+            if (p1_choose == 2 && p2_choose == 3) { textBox1.Text += "對手回復了" + dice2 + "點能量\r\n你白白損失了" + dice1 + "點能量"; pp2 += dice2 * 2; }
+            if (p1_choose == 3 && p2_choose == 2) { textBox1.Text += "你回復了" + dice1 + "點能量\r\n對手白白損失了" + dice2 + "點能量"; pp1 += dice1 * 2; }
+            if (p1_choose == 1 && p2_choose == 3) { result2 = dice1; textBox1.Text += "你對對手造成了" + result2 + "點傷害\r\n對手回復了" + dice2 + "點能量"; pp2 += dice2 * 2; }
+            if (p1_choose == 3 && p2_choose == 1) { result1 = dice2; textBox1.Text += "你回復了" + dice1 + "點能量\r\n對手對你造成了" + result1 + "點傷害"; pp1 += dice1 * 2; }
+            hp2 -= result2; hp1 -= result1; pp1 -= dice1; pp2 -= dice2;
+            Scroll(); await Task.Delay(3000);
+            Print();
+            if (hp1 > 0 && hp2 > 0 && pp1 >= 0 && pp2 >= 0)
+            {
+                textBox1.Text = "Player1現在有" + hp1 + "點血量，" + pp1 + "點能量\r\nPlayer2現在有" + hp2 + "點血量，" + pp2 + "點能量";
+                return;
+            }
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            if (hp1 <= 0) textBox1.Text += "\r\n抱歉，你已經死了!";
+            else if (pp1 < 0) textBox1.Text += "\r\n死因:精盡人亡";
+            else textBox1.Text += "\r\nYou Win!";
+            textBox1.Text += "\r\n本遊戲由夜颯製作，感謝您的遊玩";
+            Scroll();
+            textBox1.Enabled = false;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            hp1 = 24;
+            hp2 = 24;
+            pp1 = 12;
+            pp2 = 12;
+            Print();
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Text = "Player1現在有" + hp1 + "點血量，" + pp1 + "點能量\r\nPlayer2現在有" + hp2 + "點血量，" + pp2 + "點能量\r\n選擇指令來開始遊戲";
         }
     }
 }
